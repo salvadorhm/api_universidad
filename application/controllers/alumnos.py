41d72718e6000c9ca1bba73b6076b5705ecd8031
Salvador HM <salvadorhm@gmail.com>
2020-02-17 14:41:21 -0600

    action=get metodo actionGet() funcionando

--- application/controllers/alumnos.py
+++ application/controllers/alumnos.py
@@ -1,7 +1,7 @@
 import web # pip install web.py
 import app # app.py
-import csv
-import json
+import csv # CSV parser
+import json # json parser
 
 class Alumnos:
 
@@ -16,21 +16,9 @@ class Alumnos:
             data = web.input()
             if data['token'] == "1234":
                 if data['action'] == 'get':
-                    result = {} # crear diccionario vacio
-                    result['status'] = "200 Ok"
-                    with open('static/csv/alumnos.csv','r') as csvfile:
-                        reader = csv.DictReader(csvfile) # toma la 1er fila para los nombres
-                        alumnos = []
-                        for row in reader:
-                            fila = {}
-                            fila['matricula']=(row['matricula'])
-                            fila['nombre']=(row['nombre'])
-                            fila['primer_apellido']=(row['primer_apellido'])
-                            fila['segundo_apellido']=(row['segundo_apellido'])
-                            fila['carrera']=(row['carrera'])
-                            alumnos.append(fila)
-                        result['alumnos'] = alumnos
-                    return json.dumps(result) # Parsea el diccionario a json
+                    # call action_get()
+                    result = self.actionGet()
+                    return json.dumps(result)
                 else:
                     result = {} # crear diccionario vacio
                     result['status'] = "Command not found"
@@ -43,3 +31,27 @@ class Alumnos:
             result = {} # crear diccionario vacio
             result['status'] = "Values missing, sintaxis: alumnos?action=get&token=XXXX"
             return json.dumps(result)
+
+    @staticmethod
+    def actionGet():
+        print("action_get init")
+        try:
+            result = {} # crear diccionario vacio
+            result['status'] = "200 Ok"
+            with open('static/csv/alumnos.csv','r') as csvfile:
+                reader = csv.DictReader(csvfile) # toma la 1er fila para los nombres
+                alumnos = []
+                for row in reader:
+                    fila = {}
+                    fila['matricula']=(row['matricula'])
+                    fila['nombre']=(row['nombre'])
+                    fila['primer_apellido']=(row['primer_apellido'])
+                    fila['segundo_apellido']=(row['segundo_apellido'])
+                    fila['carrera']=(row['carrera'])
+                    alumnos.append(fila)
+                result['alumnos'] = alumnos
+            return result # Parsea el diccionario a json
+        except  Exception as e:
+            result = {}
+            result['status'] = "Error"
+            return result
